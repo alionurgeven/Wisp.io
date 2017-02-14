fcae9edc31f39096c51e19634613ed55e6122b34
alonge <ali.geven@ug.bilkent.edu.tr>
2017-02-15 00:46:04 +0300

    Added Constants Class
    For general and also personal usage, creating a constants class was
    needed. In order efficiently test and keep track of all of the variables
    in game Constants class will be used.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constants {
+
+	///	<description>>
+	/// Body of constants for level Manager, Pool Manager, UI Manager
+	/// for Anıl Sert 
+	/// </description>
+		
+		 
+	///	<description>>
+	/// Body of constants for Collectables
+	/// for Hayati İbiş
+	/// </description>
+
+
+	///	<description>>>
+	/// Body of constants for Ability Manager, Game Manager
+	/// for Ali Onur Geven
+	/// </description>
+
+
+	///	<description>
+	///  Body of constants for Wisp
+	///  for general use
+	/// </description>
+
+}
