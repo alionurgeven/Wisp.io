201b653183c4a9d70cf9478cac7e38848956a273
Anıl Sert <anil.sert9@gmail.com>
2017-02-15 10:37:47 +0300

    Initialize Managers class

--- Assets/Scripts/Managers/_Managers.cs
+++ Assets/Scripts/Managers/_Managers.cs
@@ -1,15 +1,38 @@
-﻿using UnityEngine;
-using System.Collections;
+using UnityEngine;
 
-public class Managers : MonoBehaviour {
+public class Managers : MonoBehaviour
+{
+    // Singleton instance of the Managers class
+    public static Managers Instance;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // All other managers to access
+    private UIManager uiManager;
+    private PoolManager poolManager;
+    private GameManager gameManager;
+    private LevelManager levelManager;
+    private AbilityManager abilityManager;
+
+    /// <summary>
+    /// Create singleton Instance if there is not any instance before
+    /// and get all the references to the other managers in Awake().
+    /// </summary>
+    private void Awake()
+    {
+        // Creating singleton instance of the Managers
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        // Get all the manager components
+        uiManager = GetComponent<UIManager>();
+        poolManager = GetComponent<PoolManager>();
+        gameManager = GetComponent<GameManager>();
+        levelManager = GetComponent<LevelManager>();
+        abilityManager = GetComponent<AbilityManager>();
+    }
 }
